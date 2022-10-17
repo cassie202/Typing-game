2e7af7e3667e650eabcba11c5e851c5585dcaee3
Cassandra Umeh <umehcassandra@gmail.com>
2022-10-17 14:03:00 +0100

    build typing-game

---
+++ script.js
@@ -0,0 +1,85 @@
+const quotes = [
+  "We cannot solve problems with the kind of thinking we employed when we came up with them",
+  "Learn as if you will live forever, live like you will die tomorrow..",
+  "Stay away from those people who try to disparage your ambitions. Small minds will always do that, but great minds will give you a feeling that you can become great too",
+  "When you give joy to other people, you get more joy in return. You should give a good thought to happiness that you can give out.",
+  "When you change your thoughts, remember to also change your world..",
+  "Nothing clears up a case so much as stating it to another person.",
+  "It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest.",
+  "Success is not final; failure is not fatal: It is the courage to continue that counts",
+];
+// store the list of words and the index of the word the player is currently typing
+let words = [];
+let wordIndex = 0;
+// the starting time
+let startTime = Date.now();
+// page elements
+const quoteElement = document.getElementById("quote");
+const messageElement = document.getElementById("message");
+const typedValueElement = document.getElementById("typed-value");
+
+document.getElementById("start").addEventListener("click", () => {
+  // get a quote
+  const quoteIndex = Math.floor(Math.random() * quotes.length);
+  const quote = quotes[quoteIndex];
+  // Put the quote into an array of words
+  words = quote.split(" ");
+  // reset the word index for tracking
+  wordIndex = 0;
+
+  const spanWords = words.map(function (word) {
+    return `<span>${word} </span>`;
+  });
+  // Convert into string and set as innerHTML on quote display
+  quoteElement.innerHTML = spanWords.join("");
+  // Highlight the first word
+  quoteElement.childNodes[0].className = "highlight";
+  // Clear any prior messages
+  messageElement.innerText = "";
+
+  // Setup the textbox
+  // Clear the textbox
+  typedValueElement.value = "";
+  // set focus
+  typedValueElement.focus();
+  // set the event handler
+
+  // Start the timer
+  startTime = new Date().getTime();
+});
+
+typedValueElement.addEventListener("input", () => {
+  // Get the current word
+  const currentWord = words[wordIndex];
+  // get the current value
+  const typedValue = typedValueElement.value;
+
+  if (typedValue === currentWord && wordIndex === words.length - 1) {
+    // end of sentence
+    // Display success
+    const elapsedTime = new Date().getTime() - startTime;
+    const message = `CONGRATULATIONS! You finished in ${
+      elapsedTime / 1000
+    } seconds.`;
+    messageElement.innerText = message;
+  } else if (typedValue.endsWith(" ") && typedValue.trim() === currentWord) {
+    // end of word
+    // clear the typedValueElement for the new word
+    typedValueElement.value = "";
+    // move to the next word
+    wordIndex++;
+    // reset the class name for all elements in quote
+    for (const wordElement of quoteElement.childNodes) {
+      wordElement.className = "";
+    }
+    // highlight the new word
+    quoteElement.childNodes[wordIndex].className = "highlight";
+  } else if (currentWord.startsWith(typedValue)) {
+    // currently correct
+    // highlight the next word
+    typedValueElement.className = "";
+  } else {
+    // error state
+    typedValueElement.className = "error";
+  }
+});
